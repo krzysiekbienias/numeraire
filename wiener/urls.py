e9f7f13c12aa06034409c4017e30a71eb745de33
krzysiekbienias <krzysztofbienias@krzysztofs-Mac-mini.local>
2024-03-24 10:01:18 +0100

    Expand local urls by new added templates. Add photo from unsplash. Rename landing_page.html file will be renamed in next commit.

--- wiener/urls.py
+++ wiener/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 app_name='wiener'
 
-urlpatterns=[path("", views.landing_page, name="landing-page")
+urlpatterns=[path("", views.landing_page, name="landing-page"),
+             path("home-page", views.home_page, name="home-page"),
+             path("home-page/eq-dashboard",views.equities,name="eq-dashboard"),
+             path("test",views.test,name="test"),
+             path("home-page/eq-dashboard/trade-book",views.trade_book,name="trade-book")
+            #  path("home-page/fx-dashboard",views.fx,name="fx-dashboard"),
+            #  path("home-page/ir-dashboard",views.ir,name="ir-dashboard")
              ]
