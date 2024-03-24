49db1a5db09af7de0aa2b87eeef3a41affb8d8c8
krzysiekbienias <krzysztofbienias@krzysztofs-Mac-mini.local>
2024-03-24 10:21:02 +0100

    Add forms.py file containing MarketForm class.

---
+++ wiener/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class MarketForm(forms.Form):
+
+    valuation_date=forms.CharField(label="Market Date",initial="2023-05-12")
+    risk_free_rate=forms.FloatField(label="Risk Free Rate",initial=0.06)
+    volatility=forms.FloatField(min_value=0,label="Volatility",initial=0.22)
