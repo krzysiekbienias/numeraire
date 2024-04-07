41961382f68ba8289e11ea33406d8f38576bc01f
krzysiekbienias <krzysztofbienias@krzysztofs-Mac-mini.local>
2024-04-07 22:00:59 +0200

    Robust forms using widget tweaks.

--- base/settings.py
+++ base/settings.py
@@ -42,10 +42,14 @@ INSTALLED_APPS = [
     #rd party apps
     'tailwind',
     'theme',
-    'django_browser_reload'
+    'django_browser_reload',
+    'widget_tweaks'
+    
 ]
 
 TAILWIND_APP_NAME = 'theme'
+
+
 
 INTERNAL_IPS = [
     "127.0.0.1",
