af6af5d0498d01c16de565f39a5253950c848960
krzysiekbienias <krzysztofbienias@krzysztofs-Mac-mini.local>
2024-04-20 21:57:42 +0200

    refactor ajax call, replace addEventListner on button.click(function) style

--- wiener/static/wiener/single_trade.js
+++ wiener/static/wiener/single_trade.js
@@ -8,7 +8,8 @@ const postForm=document.getElementById("market-data-form")
 const csrf=document.getElementsByName('csrfmiddlewaretoken')
 const pv_dict=document.getElementById("pv")
 var html_content=document.getElementById('res')
-var price=document.getElementById('price')
+var price_trade=document.getElementById('price-trade')
+
 
 
 // --------------------------
@@ -19,10 +20,8 @@ var price=document.getElementById('price')
 // End Region Form
 // --------------------------
 
-
-postForm.addEventListener('submit',e=>{
-
-    e.preventDefault()
+$("#price-trade-btn").click(function (e){
+    e.preventDefault();
     const valuation=document.getElementById('id_valuation_date')
     const risk_free_rate=document.getElementById('id_risk_free_rate')
     const volatility=document.getElementById('id_volatility')
@@ -37,68 +36,60 @@ postForm.addEventListener('submit',e=>{
         },
         dataType:'json',
         success:function(data){
-            var tableData ='<h4 class="ui horizontal divider header">'+
-                            '<i class="tag icon"></i>Pvable dictionary</h4>'+
-                            '<p>PVAble dictionary is a dictionary to store everything data for pricing.'+
+            price_trade.innerHTML=data.price_trade.toFixed(2)
 
-                             "It is a combination market data and trade attributes,"+
-                             'supported by additional calulations.</p>'+
-                            '<h4 class="ui horizontal divider header">'+
-                            '<i class="database icon"></i>Pvable'+
-                            '</h4>'
-            tableData+='<table class="ui definition table">'
-            $.each(data.pv_dict, function(key, value){
-            tableData += '<tr>';
-            tableData += '<td class="two wide column">' + key + '</td>';
-            tableData += '<td>' + value + '</td>';
-            tableData += '</tr>';
+        },
+
+        error:function(error){
+            console.log(error)
+        }
+    })
+
+})
+
+
+$("#pv-able-btn").click(function (e){
+    e.preventDefault()
+    const valuation=document.getElementById('id_valuation_date')
+    const risk_free_rate=document.getElementById('id_risk_free_rate')
+    const volatility=document.getElementById('id_volatility')
+
+    $.ajax({
+        type: "POST",
+        url: '',
+        data: {
+            "csrfmiddlewaretoken": csrf[0].value,
+            "valuation_date": valuation.value,
+            "risk_free_rate": risk_free_rate.value,
+            "volatility": volatility.value
+        },
+        dataType: 'json',
+        success: function (data) {
+            var tableData = '<h4 class="ui horizontal divider header">' +
+                '<i class="tag icon"></i>Pvable dictionary</h4>' +
+                '<p>PVAble dictionary is a dictionary to store everything data for pricing.' +
+
+                "It is a combination market data and trade attributes," +
+                'supported by additional calulations.</p>' +
+                '<h4 class="ui horizontal divider header">' +
+                '<i class="database icon"></i>Pvable' +
+                '</h4>'
+            tableData += '<table class="ui definition table">'
+            $.each(data.pv_dict, function (key, value) {
+                tableData += '<tr>';
+                tableData += '<td class="two wide column">' + key + '</td>';
+                tableData += '<td>' + value + '</td>';
+                tableData += '</tr>';
             });
             tableData += '</table>';
-            $('#res').html(tableData); 
+            $('#res').html(tableData);
 
-
-            const priceToDisplay=data.price.toFixed(2);
-            price.innerHTML=priceToDisplay;
-            
         },
-        
-        error:function(error){
+
+        error: function (error) {
             console.log(error)
         }
-    })
-})
 
-
-
-
-postForm.addEventListener('submit',e=>{
-
-    e.preventDefault()
-    const valuation=document.getElementById('id_valuation_date')
-    const risk_free_rate=document.getElementById('id_risk_free_rate')
-    const volatility=document.getElementById('id_volatility')
-    $.ajax({
-        type:"POST",
-        url:'',
-        data:{
-            "csrfmiddlewaretoken":csrf[0].value,
-            "valuation_date":valuation.value,
-            "risk_free_rate":risk_free_rate.value,
-            "volatility":volatility.value
-        },
-        dataType:'json',
-        success:function(data){
-            
-
-
-            const priceToDisplay=data.price.toFixed(2);
-            price.innerHTML=priceToDisplay;
-            
-        },
-        
-        error:function(error){
-            console.log(error)
-        }
     })
 })
 
@@ -106,9 +97,4 @@ postForm.addEventListener('submit',e=>{
 
 
 
-$(document).ready(function(){
-    $("#pv-able-btn").click(function(){
-        $("#res").toggle();
-        
-    });
-})
+
