43a5a124ab706f9e8ea50f746efdb344cb01e3b9
krzysiekbienias <krzysztofbienias@krzysztofs-Mac-mini.local>
2024-04-23 09:23:20 +0200

    Add set_year_fraction_convention ,to QuantLibToolKit. It was transfered from pricing environment.py

--- tool_kit/fundamentals.py
+++ tool_kit/fundamentals.py
@@ -1,17 +1,32 @@
 import QuantLib as ql
 from quantlib_tool_kit import QuantLibToolKit
+from app_settings import AppSettings
 
 
 class InterestRateFundamentals:
 
     @staticmethod
+    def get_year_fraction(schedule: ql.Schedule, convention) -> float:
+        lf_year_fraction = []
+        scheduled_dates = list(schedule)
+        if len(scheduled_dates) == 2:
+            # frequency probably set as 'once' so only valuation and termination date has been provided
+            return convention.yearFraction(scheduled_dates[0], scheduled_dates[1])
+        for i in range(1, len(scheduled_dates)):
+            temp_yf = convention.yearFraction(
+                scheduled_dates[i - 1], scheduled_dates[i])
+            lf_year_fraction.append(temp_yf)
+        return lf_year_fraction
+
+    @staticmethod
     def discount_factor(spot_rate: float, valuation_date: (str, ql.Date), maturity_date: (str, ql.Date),
-                        freq_period: str) -> float:
+                        freq_period: str,
+                        year_fraction_convention: str = AppSettings.DEFAULT_YEAR_FRACTION_CONVENTION) -> float:
         """
         Description
         -----------
-        Calculates the discount factor given for a given spot rate. If you use a valuation date and maturity date,as a string
-        pass them in format 'YYYY-MM-DD'
+        Calculates the discount factor given for a given spot rate. If you use a valuation date and maturity date, as
+        a string pass them in format 'YYYY-MM-DD'
 
         Parameters
         ----------
@@ -28,15 +43,11 @@ class InterestRateFundamentals:
         if isinstance(valuation_date, str) or isinstance(maturity_date, str):
             valuation_date = QuantLibToolKit.string_2ql_date(valuation_date)
             maturity_date = QuantLibToolKit.string_2ql_date(maturity_date)
-        schedule = list(QuantLibToolKit.define_schedule(valuation_date=valuation_date,
-                                                   termination_date=maturity_date,
-                                                   freq_period=freq_period))
-
-
-
-
-        time_to_maturity = valuation_date - maturity_date
-        return 1 / (1 + spot_rate) ** time_to_maturity
+        fractions = InterestRateFundamentals.get_year_fraction(
+            schedule=QuantLibToolKit.define_schedule(valuation_date=valuation_date, termination_date=maturity_date,
+                                                     freq_period=freq_period),
+            convention=QuantLibToolKit.set_year_fraction_convention(year_fraction_conv=year_fraction_convention))
+        return 1 / (1 + spot_rate) ** fractions
 
     @staticmethod
     def simple_forward_rate(valuation_date: ql.Date, expiry_date: ql.Date, maturity_date: ql.Date) -> float:
@@ -55,11 +66,7 @@ class InterestRateFundamentals:
         -------
         float
         """
-
-if __name__ == '__main__':
-    valuation_date: str = "2023-05-12"
-    maturity_date: str = "2024-05-12"
+        pass
 
 
-    InterestRateFundamentals.discount_factor(spot_rate=0.05, freq_period="annual", valuation_date=valuation_date,
-                                             maturity_date=maturity_date)
+
