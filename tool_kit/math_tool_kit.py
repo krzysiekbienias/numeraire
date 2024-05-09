cf4aa37084129b79b006b22a85b2b9d087573cd6
krzysiekbienias <krzysztofbienias@krzysztofs-Mini.lan>
2024-05-09 14:05:49 +0200

    Implement Probability wraper. Based on scipy stats. Only Normal and tStudent fammily as of now. It will be extended in the future

--- tool_kit/math_tool_kit.py
+++ tool_kit/math_tool_kit.py
@@ -1,2 +1,36 @@
+from scipy import stats as st
 
+
+
+class ProbabilityInterface:
+
+    def generate_random_numbers(self):
+        pass
+
+    def probability_density_function(self):
+        pass
+
+    def cumulative_distribution_function(self):
+        pass
+
+
+class NormalDistributionFamily(ProbabilityInterface):
+
+    @staticmethod
+    def generate_random_numbers(loc=0, scale=1, size=10):
+        return st.norm.rvs(loc=loc, scale=scale, size=size)
+
+    @staticmethod
+    def probability_density_function(x, loc=0, scale=1):
+        return st.norm.pdf(x, loc=loc, scale=scale)
+
+    @staticmethod
+    def cumulative_distribution_function(x, loc=0, scale=1):
+        return st.norm.cdf(x, loc=loc, scale=scale)
+
+
+class tStudentFamily(ProbabilityInterface):
+    @staticmethod
+    def probability_density_function(x,degree_of_freedom):
+        return st.t.pdf(x, degree_of_freedom)
 
